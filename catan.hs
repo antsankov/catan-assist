6e365ac0a7fe40992d52338acc9360c84e77308b
Alex Tsankov <antsankov@gmail.com>
2017-05-29 15:02:13 -0600

    Add data types

--- catan.hs
+++ catan.hs
@@ -1,7 +1,13 @@
 import Data.Tuple.Select
 
-type Coord = (Int, Int)
 type Settlement = [Coord]
+
+type Coord = (Int, Int)
+data Resource = Sheep | Brick | Ore | Wood | Wheat
+data Tile = Tile { loc :: Coord
+                 , resource :: Resource
+                 , score :: Int
+                 }
 
 coordList :: [Coord]
 coordList = [           ( 0,2),( 1,2),( 2,2)
@@ -11,6 +17,7 @@ coordList = [           ( 0,2),( 1,2),( 2,2)
             ,           ( -2,-2),( -1,-2), (0,-2)
             ]
 
+-- Given a hex coord, give the hexs bordered by all possible settlements.
 borders :: Coord -> [Settlement]
 borders hex =
     [    -- 0 deg
@@ -26,6 +33,8 @@ borders hex =
          -- 300 deg
     ,    [hex, (sel1 hex - 1, sel2 hex), (sel1 hex, sel2 hex + 1)]
     ]
+
+-- TODO: Generate tiles with resources, make a hashmap from tile to coord, iterate through borders to create scores.
 
 main = 
     print $ borders (0, 0)
