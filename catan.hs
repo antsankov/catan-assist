0919ab644d9cb6faa368f636d6629e88096cf8ab
Alex Tsankov <antsankov@gmail.com>
2017-08-09 00:22:11 -0400

    outputing scores for settlment

--- catan.hs
+++ catan.hs
@@ -4,7 +4,7 @@ import Data.Tuple.Select
 import Data.Map
 
 type Coord = (Int, Int)
-type Settlement = [Coord]
+type Settlement= [Coord]
 
 data Resource = Sheep | Brick | Ore | Wood | Wheat deriving (Show)
 data Tile = Tile { resource :: Resource
@@ -22,10 +22,11 @@ coordList = [           ( 0,2),( 1,2),( 2,2)
 
 testBoard :: Map Coord Tile 
 testBoard = fromList[
-                ((0,2), Tile Brick 3)
-            ,   ((1,2), Tile Ore 2)
-            ,   ((-1, 1), Tile Wheat 3)
-            ,   ((0, 1), Tile Sheep 5)
+                ((0,2), Tile Brick 10)
+            ,   ((1,2), Tile Ore 100)
+            ,   ((2,2), Tile Brick 12)
+            ,   ((-1, 1), Tile Wheat 15)
+            ,   ((0, 1), Tile Sheep 6)
             ]
 
 -- Given a hex coord, give the hexs bordered by all possible settlements.
@@ -46,11 +47,6 @@ possibleSettlements hex =
     ]
 
 
--- Extract the aggregate score for an array of tiles.
-calcSettlementValue :: [Coord] -> Int
-calcSettlementValue tiles =
-    sum $ Prelude.map extractScore tiles
-
 -- Given a coordinate, get the score from it.
 extractScore :: Coord -> Int
 extractScore coord =
@@ -58,13 +54,18 @@ extractScore coord =
         Nothing -> 0
         Just tile -> score tile
 
+-- Extract the aggregate score for an array of tiles.
+calcSettlementValue :: [Coord] -> [Int]
+calcSettlementValue coords =
+    Prelude.map extractScore coords
+
 -- Given a title, find the value of all possible settlements
-appraiseTile :: Coord -> Int
+appraiseTile :: Coord -> [[Int]]
 appraiseTile tileCoord = 
     let
         possible = possibleSettlements(tileCoord)
     in
-        maximum $ Prelude.map calcSettlementValue possible
+        Prelude.map calcSettlementValue possible
 
 main = 
     -- print $ possibleSettlements (0, 1)
